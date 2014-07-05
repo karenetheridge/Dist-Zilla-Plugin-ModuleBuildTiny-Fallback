c1344b7ace3749b27aea1b6292cd74f15935bb0d
Karen Etheridge <ether@cpan.org>
2014-07-04 22:50:34 -0700

    add keywords to metadata

--- lib/Dist/Zilla/Plugin/ModuleBuildTiny/Fallback.pm
+++ lib/Dist/Zilla/Plugin/ModuleBuildTiny/Fallback.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Plugin::ModuleBuildTiny::Fallback;
 # ABSTRACT: Build a Build.PL that uses Module::Build::Tiny, falling back to Module::Build as needed
+# KEYWORDS: plugin installer Module::Build Build.PL toolchain legacy ancient backcompat
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Moose;
